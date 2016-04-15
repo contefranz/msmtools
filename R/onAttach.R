b71b747380f9febe20b428bd49fb0d49adac2adc
Francesco Grossetti <contefranz@gmail.com>
2016-04-15 16:14:00 +0200

    Added package docs. Now ?msmtools works. Fixed dependency declaration on survival in .onAttach()

--- R/onAttach.R
+++ R/onAttach.R
@@ -3,6 +3,6 @@
   if ( interactive() ) {
     packageStartupMessage( 'Welcome to msmtools ', as.character( packageVersion( "msmtools" ) ),
                            '! An easy way to build specific longitudinal datasets to be used with msm' )
-    packageStartupMessage( 'msmtools requires data.table (>= 1.9.6), msm (>= 1.6.0) and survival (2.38.0)' )
+    packageStartupMessage( 'msmtools requires data.table (>= 1.9.6), msm (>= 1.6.0) and survival (>= 2.38.0)' )
   }
 }
