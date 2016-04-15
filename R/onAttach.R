f5aac0b0431f36ab140bb5248563837f3dd19730
Francesco Grossetti <contefranz@gmail.com>
2016-04-15 12:32:46 +0200

    Added internal onAttach function to display useful infos when library() or require() are called.

---
+++ R/onAttach.R
@@ -0,0 +1,8 @@
+.onAttach = function( libname, pkgname ) {
+  # Runs when attached to search() path such as by library() or require()
+  if ( interactive() ) {
+    packageStartupMessage( 'Welcome to msmtools ', as.character( packageVersion( "msmtools" ) ),
+                           '! An easy way to build specific longitudinal datasets to be used with msm' )
+    packageStartupMessage( 'msmtools requires data.table (>= 1.9.6), msm (>= 1.6.0) and survival (2.38.0)' )
+  }
+}
