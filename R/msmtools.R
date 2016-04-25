adad7cd823f87baea5929a4f7f3d72c2f1f2c79f
Francesco Grossetti <contefranz@gmail.com>
2016-04-25 20:47:32 +0200

    Rephrased msmtools general to bring it similar to vignette and readme

--- R/msmtools.R
+++ R/msmtools.R
@@ -1,7 +1,8 @@
-#' msmtools: a companion to msm package
+#' Building augmented data for multi-state models: the \code{msmtools} package
 #'
-#' Make it easier to build longitudinal datasets feasible for the analyses in the
-#' context of multi-state models as computed by \code{msm} package.
+#' \code{msmtools} introduces a fast and general method for restructuring classical longitudinal datasets
+#' into \emph{augmented} ones. The reason for this is to facilitate the modeling of longitudinal data
+#' under a multi-state framework using the \code{msm} package.
 #' It also provides two graphical goodness-of-fit tools on estimated survival and prevalences
 #' to carry out model assessment under Markov assumption.
 #'
