4170d9f42f4d81a63d6842f7ba8ad98e0e7ef616
Francesco Grossetti <francesco.grossetti@unibocconi.it>
2026-05-26 09:07:51 +0200

    Normalize polish syntax style

--- R/polish.R
+++ R/polish.R
@@ -1,5 +1,5 @@
-if ( getRversion() >= "2.15.1" ) {
-  utils::globalVariables( c( ":=", ".I", ".N", "index" ) )
+if (getRversion() >= "2.15.1") {
+  utils::globalVariables(c(":=", ".I", ".N", "index"))
 }
 
 #' Remove observations with different states occurring at the same time
@@ -38,109 +38,109 @@ if ( getRversion() >= "2.15.1" ) {
 #' @examples
 #'
 #' # loading data
-#' data( hosp )
+#' data(hosp)
 #'
 #' # augmenting longitudinal data
-#' hosp_aug = augment( data = hosp, data_key = subj, n_events = adm_number,
-#'                     pattern = label_3, t_start = dateIN, t_end = dateOUT,
-#'                     t_cens = dateCENS )
+#' hosp_aug = augment(data = hosp, data_key = subj, n_events = adm_number,
+#'                    pattern = label_3, t_start = dateIN, t_end = dateOUT,
+#'                    t_cens = dateCENS)
 #'
 #' # cleaning targeted duplicate transitions
-#' hosp_aug_clean = polish( data = hosp_aug, data_key = subj, pattern = label_3 )
+#' hosp_aug_clean = polish(data = hosp_aug, data_key = subj, pattern = label_3)
 #'
 #' @author Francesco Grossetti <francesco.grossetti@unibocconi.it>.
 #' @importFrom data.table setDT setkey setkeyv rbindlist uniqueN
 #' @export
 
-polish = function( data, data_key, pattern, time = NULL, check_NA = FALSE,
-                   copy = FALSE,
-                   verbosity = getOption( "msmtools.verbosity", "quiet" ) ) {
+polish = function(data, data_key, pattern, time = NULL, check_NA = FALSE,
+                  copy = FALSE,
+                  verbosity = getOption("msmtools.verbosity", "quiet")) {
 
   tic = proc.time()
-  verbosity = .msmtools_verbosity( verbosity )
-  .msmtools_validate_flag( copy, "copy" )
-  .msmtools_validate_flag( check_NA, "check_NA" )
+  verbosity = .msmtools_verbosity(verbosity)
+  .msmtools_validate_flag(copy, "copy")
+  .msmtools_validate_flag(check_NA, "check_NA")
 
-  if ( missing( data ) ) {
-    stop( 'a dataset of class data.table or data.frame must be provided' )
+  if (missing(data)) {
+    stop('a dataset of class data.table or data.frame must be provided')
   }
-  if ( !inherits( data, "data.table" ) && !inherits( data, "data.frame" ) ) {
-    stop( "a dataset of class data.table or data.frame must be provided" )
+  if (!inherits(data, "data.table") && !inherits(data, "data.frame")) {
+    stop("a dataset of class data.table or data.frame must be provided")
   }
-  if ( missing( data_key ) ) {
-    stop( 'a variable of keying must be provided' )
+  if (missing(data_key)) {
+    stop('a variable of keying must be provided')
   }
-  if ( missing( pattern ) ) {
-    stop( "a pattern must be provided" )
+  if (missing(pattern)) {
+    stop("a pattern must be provided")
   }
-  if ( copy ) {
-    data = data.table::copy( data )
+  if (copy) {
+    data = data.table::copy(data)
   }
-  if ( inherits( data, 'data.frame' ) ) {
-    setDT( data )
+  if (inherits(data, 'data.frame')) {
+    setDT(data)
   }
-  .msmtools_cli_rule( verbosity, "setting everything up" )
+  .msmtools_cli_rule(verbosity, "setting everything up")
 
-  setkey( data, NULL )
-  cols = as.character( substitute( data_key ) )
-  if ( !length( cols ) ) {
-    cols = colnames( data )
+  setkey(data, NULL)
+  cols = as.character(substitute(data_key))
+  if (!length(cols)) {
+    cols = colnames(data)
   }
-  pattern = as.character( substitute( pattern ) )
-  time_arg = substitute( time )
-  if ( missing( time ) || is.null( time_arg ) ) {
-    time = .polish_resolve_time( data )
-    .msmtools_cli_info( verbosity, paste0( time, " set as time variable" ) )
+  pattern = as.character(substitute(pattern))
+  time_arg = substitute(time)
+  if (missing(time) || is.null(time_arg)) {
+    time = .polish_resolve_time(data)
+    .msmtools_cli_info(verbosity, paste0(time, " set as time variable"))
   } else {
-    time = as.character( time_arg )
+    time = as.character(time_arg)
   }
-  .polish_check_columns( data, c( cols, pattern, time ) )
-  setkeyv( data, cols )
+  .polish_check_columns(data, c(cols, pattern, time))
+  setkeyv(data, cols)
 
-  if ( isTRUE( check_NA ) ) {
+  if (isTRUE(check_NA)) {
     .msmtools_cli_info(
       verbosity,
       "checking for any missing values in function arguments"
     )
-    checks = c( cols, pattern, time )
-    test = apply( data[ , checks, with = FALSE ], 2,
-                  function( x ) any( sum( is.na( x ) ) > 0 ) )
-    if ( any ( test ) ) {
-      missing_values = paste( names( test[ test == TRUE ] ), collapse = ", " )
-      stop( paste0( "missing values detected in: ", missing_values ) )
+    checks = c(cols, pattern, time)
+    test = apply(data[, checks, with = FALSE], 2,
+                 function(x) any(sum(is.na(x)) > 0))
+    if (any(test)) {
+      missing_values = paste(names(test[test == TRUE]), collapse = ", ")
+      stop(paste0("missing values detected in: ", missing_values))
     } else {
-      .msmtools_cli_success( verbosity, "no missing values detected" )
+      .msmtools_cli_success(verbosity, "no missing values detected")
     }
   }
 
-  data[ , index := sequence( .N ) ]
-  n_patients = uniqueN( data[[ cols[[ 1 ]] ]] )
-  values = .polish_pattern_values( data, pattern, verbosity )
+  data[, index := sequence(.N)]
+  n_patients = uniqueN(data[[cols[[1]]]])
+  values = .polish_pattern_values(data, pattern, verbosity)
 
-  alive = data[ get( pattern ) == values[ 1 ] ]
-  alive.last = alive[ alive[ , .I[ .N ], by = eval( cols ) ]$V1 ]
-  setkey( alive.last, index )
-  setkey( alive, index )
-  alive.no.last = alive[ !alive.last ]
+  alive = data[get(pattern) == values[1]]
+  alive.last = alive[alive[, .I[.N], by = eval(cols)]$V1]
+  setkey(alive.last, index)
+  setkey(alive, index)
+  alive.no.last = alive[!alive.last]
 
-  if ( length( values ) == 2 ) {
-    dead = data[ get( pattern ) == values[ 2 ] ]
-  } else if ( length( values ) == 3 ) {
-    dead = data[ get( pattern ) != values[ 1 ] ]
+  if (length(values) == 2) {
+    dead = data[get(pattern) == values[2]]
+  } else if (length(values) == 3) {
+    dead = data[get(pattern) != values[1]]
   }
 
-  l = list( alive.no.last, dead )
-  data.no.last.event = rbindlist( l )
-  row.duplicated = duplicated( data.no.last.event,
-                               by = c( eval( cols ), eval( time ) ) )
-  duplicated = data.no.last.event[ row.duplicated == TRUE ]
-  n_duplicated = uniqueN( duplicated[[ cols[[ 1 ]] ]] )
-  setkeyv( duplicated, cols )
+  l = list(alive.no.last, dead)
+  data.no.last.event = rbindlist(l)
+  row.duplicated = duplicated(data.no.last.event,
+                              by = c(eval(cols), eval(time)))
+  duplicated = data.no.last.event[row.duplicated == TRUE]
+  n_duplicated = uniqueN(duplicated[[cols[[1]]]])
+  setkeyv(duplicated, cols)
 
-  if ( n_duplicated == 0 ) {
+  if (n_duplicated == 0) {
     .msmtools_cli_success(
       verbosity,
-      paste0( "no duplicated occurrences found according to ", time )
+      paste0("no duplicated occurrences found according to ", time)
     )
   } else {
     .msmtools_cli_info(
@@ -150,13 +150,13 @@ polish = function( data, data_key, pattern, time = NULL, check_NA = FALSE,
         " subjects with at least one duplicated occurrence according to ", time
       )
     )
-    data.clean = data[ !duplicated ]
-    n_patients.to.keep = uniqueN( data.clean[[ cols[[ 1 ]] ]] )
+    data.clean = data[!duplicated]
+    n_patients.to.keep = uniqueN(data.clean[[cols[[1]]]])
     .msmtools_cli_success(
       verbosity,
       paste0(
         n_patients.to.keep, " subjects retained, corresponding to ",
-        round( 100 * ( n_patients.to.keep / n_patients ), 2 ), "%"
+        round(100 * (n_patients.to.keep / n_patients), 2), "%"
       )
     )
     .msmtools_cli_success(
@@ -165,22 +165,22 @@ polish = function( data, data_key, pattern, time = NULL, check_NA = FALSE,
     )
   }
 
-  data[ , index := NULL ]
-  if ( n_duplicated > 0 ) {
-    data.clean[ , index := NULL ]
+  data[, index := NULL]
+  if (n_duplicated > 0) {
+    data.clean[, index := NULL]
   }
   elapsed = proc.time() - tic
   .msmtools_cli_rule(
     verbosity,
-    paste0( "polish() took: ", elapsed[ 3 ], " sec." )
+    paste0("polish() took: ", elapsed[3], " sec.")
   )
 
-  if ( n_duplicated == 0 ) {
+  if (n_duplicated == 0) {
     data[]
-    return( data )
+    return(data)
   }
   data.clean[]
-  return( data.clean )
+  return(data.clean)
 }
 
 #' Resolve the default duplicate-time column for `polish()`
@@ -190,14 +190,14 @@ polish = function( data, data_key, pattern, time = NULL, check_NA = FALSE,
 #'
 #' @keywords internal
 #' @noRd
-.polish_resolve_time = function( data ) {
-  if ( "augmented_int" %in% names( data ) ) {
-    return( "augmented_int" )
+.polish_resolve_time = function(data) {
+  if ("augmented_int" %in% names(data)) {
+    return("augmented_int")
   }
-  if ( "augmented_num" %in% names( data ) ) {
-    return( "augmented_num" )
+  if ("augmented_num" %in% names(data)) {
+    return("augmented_num")
   }
-  stop( "time must be provided when data does not contain augmented_int or augmented_num" )
+  stop("time must be provided when data does not contain augmented_int or augmented_num")
 }
 
 #' Check that required `polish()` columns exist
@@ -207,13 +207,13 @@ polish = function( data, data_key, pattern, time = NULL, check_NA = FALSE,
 #'
 #' @keywords internal
 #' @noRd
-.polish_check_columns = function( data, columns ) {
-  missing_columns = setdiff( columns, names( data ) )
-  if ( length( missing_columns ) ) {
+.polish_check_columns = function(data, columns) {
+  missing_columns = setdiff(columns, names(data))
+  if (length(missing_columns)) {
     stop(
       paste0(
         "the following columns are not present in data: ",
-        paste( missing_columns, collapse = ", " )
+        paste(missing_columns, collapse = ", ")
       )
     )
   }
@@ -226,18 +226,18 @@ polish = function( data, data_key, pattern, time = NULL, check_NA = FALSE,
 #'
 #' @keywords internal
 #' @noRd
-.polish_pattern_values = function( data, pattern, verbosity ) {
-  values = sort( unique( data[[ pattern ]] ) )
-  if ( !length( values ) %in% 2:3 ) {
-    stop( "pattern must have 2 or 3 unique values" )
+.polish_pattern_values = function(data, pattern, verbosity) {
+  values = sort(unique(data[[pattern]]))
+  if (!length(values) %in% 2:3) {
+    stop("pattern must have 2 or 3 unique values")
   }
   .msmtools_cli_info(
     verbosity,
-    paste0( "checking ", pattern, " and defining patterns" )
+    paste0("checking ", pattern, " and defining patterns")
   )
   .msmtools_cli_success(
     verbosity,
-    paste0( "detected ", length( values ), " values in ", pattern )
+    paste0("detected ", length(values), " values in ", pattern)
   )
   values
 }
